9edaa8f9ccaeb161971ed0f17917adaea5197034
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-01-16 23:30:25 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+
+class Solution:
+    def isInterleave(self, a: str, b: str, target: str):
+        n, m = len(a), len(b)
+
+        @lru_cache(maxsize=None)
+        def dp(i: int, j: int):
+            if i == n and j == m:
+                return True
+            return any([
+                i < n and a[i] == target[i + j] and dp(i + 1, j),
+                j < m and b[j] == target[i + j] and dp(i, j + 1),
+            ])
+
+        return n + m == len(target) and dp(0, 0)
+
